8551575ed7929448e667e7f0b7dfdbee1d699c84
LukeBrantley <27262117+LukeBrantley@users.noreply.github.com>
2021-02-13 16:56:28 -0500

    new branch to clean data placement

--- code/combine_clean_data.py
+++ code/combine_clean_data.py
@@ -2,16 +2,15 @@ import pandas as pd
 import os
 from pathlib import Path
 
-def read_clean_data():
-    curr_filepath = os.path.split(__file__)[0]
-    data_filepath = Path(curr_filepath).parents[0] / 'data/Heart_Disease_Mortality_Data_Among_US_Adults__35___by_State_Territory_and_County___2016-2018.csv'
+def read_clean_data(data_folder):
+    data_filepath = data_folder + '/Heart_Disease_Mortality_Data_Among_US_Adults__35___by_State_Territory_and_County___2016-2018.csv'
 
     df = pd.read_csv(data_filepath)
 
-    strats_df = df[(df.Stratification1!='Overall') | (df.Stratification2!='Overall')]
+    strats_df = df[(df['Stratification1'] != 'Overall') | (df['Stratification2'] != 'Overall')]
 
-    counties = df[df.GeographicLevel == 'County']
-    counties.shape
+    counties = df[df['GeographicLevel'] == 'County']
+    #counties.shape
 
     t = counties.iloc[1,:]
 
@@ -24,10 +23,11 @@ def read_clean_data():
         'Data_Value':'mortality_rt', 'Stratification1':'gender', 
         'Stratification2':'race'}, inplace=True)
 
-    redux['y'] = redux.mortality_rt / 100000
+    redux['y'] = redux['mortality_rt'] / 100000
 
     return redux
 
 if __name__ == '__main__':
-    clean_data = read_clean_data()
-    clean_data.to_csv(os.path.split(__file__)[0] + '\clean_data.csv')
+    data_filepath = str(Path(os.path.split(__file__)[0]).parents[0] / 'data/')
+    clean_data = read_clean_data(data_filepath)
+    clean_data.to_csv(data_filepath + '/mortality_rt_data.csv')
